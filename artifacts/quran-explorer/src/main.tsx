64d3f325c0b9d3a03670687e496c795ce8bc2d92
bintyousaf244-svg <bintyousaf244@gmail.com>
2026-05-08 13:44:53 +0500

    Update main.tsx

--- artifacts/quran-explorer/src/main.tsx
+++ artifacts/quran-explorer/src/main.tsx
@@ -5,8 +5,7 @@ import App from "./App";
 import "./index.css";
 
 // Connect frontend to Render backend
-setBaseUrl("https://quranic-linguistic-explorer.onrender.com/api");
-
+setBaseUrl("https://quranic-linguistic-explorer.onrender.com");
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
