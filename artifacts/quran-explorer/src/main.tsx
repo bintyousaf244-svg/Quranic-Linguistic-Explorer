709af75197626cf504a439fbba08c2b243e3305a
bintyousaf244-svg <bintyousaf244@gmail.com>
2026-08-24 13:06:05 +0500

    Revert "commit"
    This reverts commit 13a9c25f7b561410af5d8c0a4f2314540d0ea616.

--- artifacts/quran-explorer/src/main.tsx
+++ artifacts/quran-explorer/src/main.tsx
@@ -4,7 +4,8 @@ import { setBaseUrl } from "@workspace/api-client-react";
 import App from "./App";
 import "./index.css";
 
-
+// Connect frontend to Render backend
+setBaseUrl("https://quranic-linguistic-explorer.onrender.com");
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
